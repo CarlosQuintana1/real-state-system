bed82549dd80100c9f7fa324eedad6a5f6ba39b9
Alexis Serrato <alexisserrato@MacBook-Air-de-Alexis.local>
2025-09-25 16:33:07 -0600

    [Models]: Added data validation for properties and companies

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from models.user import User
+from models.user import User, Property, Company
 
 app = FastAPI()
 
@@ -10,3 +10,11 @@ async def root():
 @app.post("/users")
 async def create_user(user_data: User):
     return user_data
+
+@app.post("/properties")
+async def create_real_state(property_data: Property):
+    return property_data
+
+@app.post("/companies")
+async def create_company(company_data: Company):
+    return company_data
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,5 +1,24 @@
 from pydantic import BaseModel, EmailStr
+from typing import Literal
 
 class User(BaseModel):
 	name: str
 	email: EmailStr
+
+class Property(BaseModel):
+	id: int
+	price: float
+	address: str
+	model_type: str
+	status: Literal["available", "sold", "rented"]
+
+class Company(BaseModel):
+	id : int
+	name: str
+	agent : list[User]
+	properties : list[Property]
+	
+	@property
+	def total_properties(self) -> int:
+		return len(self.properties)
+	#print(company.total_properties())
