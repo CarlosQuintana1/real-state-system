36bf3c4d07803a11ad136c3c905540c2c8a4243e
Alexis Serrato <alexisserrato@MacBook-Air-de-Alexis.local>
2025-11-06 14:50:29 -0600

    feat [Arquitecture/Frontend]: Changed into Header structure

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,20 +1,20 @@
 export default function Header(){
     return (
-        <header className="flex bg-[#252525] text-white text-md">
-            <div className="">
-                <div className="">
-                    <img src="https://century21mexico.com/img/c21mx/c21MexWebRGold.svg" alt="Century 21 Logo"/>
+        <header className="w-full sticky top-0 z-50 flex bg-[#252525] text-white text-md justify-items-center p-1" draggable={false}>
+            <div className="w-full grid grid-cols-3 items-center">
+                <div className="w-1/3 pl-4">
+                    <img src="https://century21mexico.com/img/c21mx/c21MexWebRGold.svg" alt="Century 21 Logo" draggable={false}/>
                 </div>
-                <nav className="">
-                    <ul className="">
-                        <li><a href="#">Propiedades</a></li>
-                        <li><a href="#">Servicios</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </nav>
-                <div className="">
-                    <a href="#" className="bg-[#] text-white p-2">Iniciar Sesion</a>
-                    <a href="#" className="bg-black text-white p-2">Registrar</a>
+                    <nav className="flex justify-center">
+                        <ul className="flex flex-row gap-6 items-center">
+                            <li><a href="#">Propiedades</a></li>
+                            <li><a href="#">Servicios</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </nav>
+                <div className="flex justify-end pr-4">
+                    <a href="#" className="text-white p-2">Iniciar Sesion</a>
+                    <a href="#" className="text-white p-2">Registrar</a>
                 </div>
             </div>
         </header>
